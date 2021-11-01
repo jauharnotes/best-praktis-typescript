0abbec8d26cda0cdd464ef5be678ee1e5aea3867
Jauharuddin <jauharnotes@gmail.com>
2021-11-01 22:44:59 +0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,1 +1,30 @@
-console.log("tes tyscript");
+let num: number = 5;
+let name: string = 'Belajar Typescript';
+let isPresent: boolean = true;
+
+//oop
+class Car {
+  model: String;
+  doors: Number;
+  isElectric: Boolean;
+  constructor(model: String, doors: Number, isElectric: Boolean) {
+    this.model = model;
+    this.doors = doors;
+    this.isElectric = isElectric;
+  }
+  displayMake(): void {
+    console.log(`This car is ${this.model}`);
+  }
+}
+
+//Interface
+interface ICar {
+        model: String,
+        make: String,
+        display(): void
+       }
+     const Car: ICar = {
+        model: 'Prius',
+        make: 'Toyota',
+        display() => { console.log('hi'); }
+      }
